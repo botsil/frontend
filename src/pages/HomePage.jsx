15802e5dd97872b35767ff7bc12665bf98de456a
Marcos Schlick <marcosschlick@gmail.com>
2025-05-24 15:44:14 -0300

    feat: setup initial frontend structure with pages and routing

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="min--screen min-h-screen bg-slate-600 flex justify-center p-6">
+      <h1>HomePage</h1>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/MembersPage.jsx
@@ -0,0 +1,9 @@
+function MembersPage() {
+  return (
+    <div className="min-w-screen min-h-screen bg-slate-600 flex justify-center p-6">
+      <h1>MembersPage</h1>
+    </div>
+  );
+}
+
+export default MembersPage;
